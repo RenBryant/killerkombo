2d1c5be056bcccd2a50e6d99fe7092a2c2468854
Emmanuel6093 <elorenzo6093@gmail.com>
2022-07-17 15:00:21 -0400

    added some test frontend files

--- server/models/User.js
+++ server/models/User.js
@@ -37,6 +37,6 @@ userSchema.methods.isCorrectPassword = async function(password) {
   return await bcrypt.compare(password, this.password);
 };
 
-const User = mongoose.model("User", userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
