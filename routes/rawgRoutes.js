871248dbbebe8cc20999f2e99653701bdb73f10a
Emmanuel6093 <elorenzo6093@gmail.com>
2022-07-17 14:35:50 -0400

    added genre routes

--- routes/rawgRoutes.js
+++ routes/rawgRoutes.js
@@ -1,14 +1,35 @@
-const axios = require('axios');
+// const axios = require('axios');
 
-module.exports = function(app) {
-    app.get('/api/rawg/games', function(req, res) {
-        axios.get('https://api.rawg.io/api/games=?page_size=12=API_KEY=')
-        .then(function(response) {
-            res.json(response.data);
-        })
-        .catch(function(error) {
-            console.log(error);
-        }
-        );
-    }
-)};
+// module.exports = function (app) {
+//     app.get("/api/games", (req, res) => {
+//         axios.get("https://api.rawg.io/api/games?" + API_KEY).then(results => {
+//             let data = results.data
+//             res.json(data)
+//         }).catch(err => {
+//             res.json(err)
+//         })
+//     })
+
+//     app.get("/api/games/:id", (req, res) => {
+//         axios.get("https://api.rawg.io/api/games?" + req.params.id + API_KEY).then(results => {
+//             let data = results.data
+//             res.json(data)
+//         }).catch(err => {
+//             res.json(err)
+//         })
+//     }    
+//     )
+// }
+
+//     app.get("/api/genres", (req, res) => {
+//         axios.get("https://api.rawg.io/api/genres?" + API_KEY).then(results => {
+//             let data = results.data
+//             res.json(data)
+//         }).catch(err => {
+//             res.json(err)
+//         }
+//     )
+//     }
+//     )
+
+//     app.get
