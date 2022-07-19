1bfc9f8e5e71b5db8c0925f48002b5d390c7e43c
Emmanuel6093 <elorenzo6093@gmail.com>
2022-07-18 20:24:52 -0400

    genre id

--- server/routes/rawgRoutes.js
+++ server/routes/rawgRoutes.js
@@ -5,7 +5,7 @@ const API_KEY = process.env.RAWG_API_KEY;
 
 module.exports = function (app) {
     app.get("/api/games", (req, res) => {
-        axios.get("https://api.rawg.io/api/games?key=" + API_KEY).then(results => {
+        axios.get("https://api.rawg.io/api/games?page_size=3&key=" + API_KEY).then(results => {
             let data = results.data
             res.json(data)
         }).catch(err => {
@@ -33,6 +33,16 @@ module.exports = function (app) {
         }
     )
     }
+    )
+
+    app.get("/api/genres/:id", (req, res) => {
+        axios.get(`https://api.rawg.io/api/genres/${req.params.id}?key=` + API_KEY).then(results => {
+            let data = results.data
+            res.json(data)
+        }).catch(err => {
+            res.json(err)
+        })
+    }    
     )
 
 };
