5b04f68ad33ef07696a503e34c0a4010eb02a8d2
Emmanuel6093 <elorenzo6093@gmail.com>
2022-07-19 21:59:11 -0400

    grid system setup for games

--- client/src/TopGames.js
+++ client/src/TopGames.js
@@ -15,27 +15,71 @@ const TopGames = () => {
     .then(resp => resp.json())
     .then(({results}) => setGames(results))
   }
-
+    // <div>
+    //   <ul>
+    //   {
+    //     games.map(game => (
+    //       <li key={game.id}>
+    //         <Link to={{
+    //             pathname: `/game/${game.name}`,
+    //             gameProps:{
+    //               game: game
+    //             }
+    //           }}>
+    //         <h3>{game.name}</h3>
+    //         <img src={game.background_image} alt="game"/>
+    //         </Link>
+    //       </li>
+    //     ))
+    //   }
+    //   </ul>
+    // </div>
   return (
-    <div>
-      <ul>
-      {
-        games.map(game => (
-          <li key={game.id}>
-            <Link to={{
-                pathname: `/game/${game.name}`,
-                gameProps:{
-                  game: game
-                }
-              }}>
-            <h3>{game.name}</h3>
-            <img src={game.background_image} alt="game"/>
-            </Link>
-          </li>
-        ))
-      }
-      </ul>
-    </div>
+<>
+<div class="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pt-6 gap-8">
+                    {/* <!-- Remove class [ h-24 ] when adding a card block --> */}
+                    {/* <!-- Remove class [ border-gray-300  dark:border-gray-700 border-dashed border-2 ] to remove dotted border --> */}
+                    <div class="rounded border-gray-300 dark:border-gray-700 border-dashed border-2 h-24"></div>
+                    {/* <!-- Remove class [ h-24 ] when adding a card block --> */}
+                    {/* <!-- Remove class [ border-gray-300  dark:border-gray-700 border-dashed border-2 ] to remove dotted border --> */}
+                    <div class="rounded border-gray-300 dark:border-gray-700 border-dashed border-2 h-24"></div>
+                    {/* <!-- Remove class [ h-24 ] when adding a card block --> */}
+                    {/* <!-- Remove class [ border-gray-300  dark:border-gray-700 border-dashed border-2 ] to remove dotted border --> */}
+                    <div class="rounded border-gray-300 dark:border-gray-700 border-dashed border-2 h-24"></div>
+                    {/* <!-- Remove class [ h-24 ] when adding a card block --> */}
+                    {/* <!-- Remove class [ border-gray-300  dark:border-gray-700 border-dashed border-2 ] to remove dotted border --> */}
+                    <div class="rounded border-gray-300 dark:border-gray-700 border-dashed border-2 h-24"></div>
+                </div>
+                <div class="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pt-6 gap-8">
+                    {/* <!-- Remove class [ h-24 ] when adding a card block --> */}
+                    {/* <!-- Remove class [ border-gray-300  dark:border-gray-700 border-dashed border-2 ] to remove dotted border --> */}
+                    <div class="rounded border-gray-300 dark:border-gray-700 border-dashed border-2 h-24"></div>
+                    {/* <!-- Remove class [ h-24 ] when adding a card block --> */}
+                    {/* <!-- Remove class [ border-gray-300  dark:border-gray-700 border-dashed border-2 ] to remove dotted border --> */}
+                    <div class="rounded border-gray-300 dark:border-gray-700 border-dashed border-2 h-24"></div>
+                    {/* <!-- Remove class [ h-24 ] when adding a card block --> */}
+                    {/* <!-- Remove class [ border-gray-300  dark:border-gray-700 border-dashed border-2 ] to remove dotted border --> */}
+                    <div class="rounded border-gray-300 dark:border-gray-700 border-dashed border-2 h-24"></div>
+                    {/* <!-- Remove class [ h-24 ] when adding a card block --> */}
+                    {/* <!-- Remove class [ border-gray-300  dark:border-gray-700 border-dashed border-2 ] to remove dotted border --> */}
+                    <div class="rounded border-gray-300 dark:border-gray-700 border-dashed border-2 h-24"></div>
+                </div>
+                <div class="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pt-6 gap-8">
+                    {/* <!-- Remove class [ h-24 ] when adding a card block --> */}
+                    {/* <!-- Remove class [ border-gray-300  dark:border-gray-700 border-dashed border-2 ] to remove dotted border --> */}
+                    <div class="rounded border-gray-300 dark:border-gray-700 border-dashed border-2 h-24"></div>
+                    {/* <!-- Remove class [ h-24 ] when adding a card block --> */}
+                    {/* <!-- Remove class [ border-gray-300  dark:border-gray-700 border-dashed border-2 ] to remove dotted border --> */}
+                    <div class="rounded border-gray-300 dark:border-gray-700 border-dashed border-2 h-24"></div>
+                    {/* <!-- Remove class [ h-24 ] when adding a card block --> */}
+                    {/* <!-- Remove class [ border-gray-300  dark:border-gray-700 border-dashed border-2 ] to remove dotted border --> */}
+                    <div class="rounded border-gray-300 dark:border-gray-700 border-dashed border-2 h-24"></div>
+                    {/* <!-- Remove class [ h-24 ] when adding a card block --> */}
+                    {/* <!-- Remove class [ border-gray-300  dark:border-gray-700 border-dashed border-2 ] to remove dotted border --> */}
+                </div>
+            
+            
+</>
   )
 }
 
