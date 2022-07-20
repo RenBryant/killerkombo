a97ce63f332417cc43bc73d70306dc2a2f8bd616
Emmanuel6093 <elorenzo6093@gmail.com>
2022-07-19 20:26:38 -0400

    navbar complete

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,6 @@ import GameDetail from "./GameDetails";
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-
 import './index.css';
 
 const httpLink = createHttpLink({
@@ -44,7 +43,6 @@ function App() {
       <div className="App">
         <Nav />
         <Switch>
-          {/* <Route path='/home' element={Home} /> */}
           <Route exact path='/' component={Home} />
           <Route path='/login' component={Login} />
           <Route path='/signup' component={Signup} />
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,14 +3,25 @@ import React from 'react';
 import Auth from '../utils/auth';
 import { useQuery } from '@apollo/client';
 
+import Nav from '../Nav';
+import { Link } from 'react-router-dom';
+
 const Home = () => {
 
 
   const loggedIn = Auth.loggedIn();
 
   return (
-    <main>
-      <h1>Home</h1>
+    <main className='min-h-screen flex flex-col justify-between'>
+      <section className='page flex-1 flex-col gap-10 lg:flex-row justify-around items-center'>
+        <section className='text-text space-y-5 text-center 2xl:text-left'>
+          <p className='text-base'>Are you done playing?</p>
+          <h1 className='text-4xl font-semibold'>Explore and review games</h1>
+          <Link to='/topgames'>
+            <button className='btn btn-primary'>Top Games</button>
+          </Link>
+        </section>
+      </section>
     </main>
   );
 };
